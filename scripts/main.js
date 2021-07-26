f7389a730aac395fc54ab56415fba0ccfa268f36
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-07-26 08:49:30 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
+// Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
 var inp_as = document.getElementById("slider");
 var array_size = inp_as.value;
 var inp_gen = document.getElementById("generateArray");
@@ -7,26 +7,25 @@ var drop_button = document.getElementById("dropbtn");
 var butts_algos = document.querySelectorAll(".algos");
 var sort_button = document.getElementById("sort");
 var dropContent = document.getElementById("dropContent");
-var div_sizes=[];
-var divs=[];
+var div_sizes = [];
+var divs = [];
 var margin_size;
 var algorithm;
 var cont = document.getElementById("arrayContainer");
-
 
 inp_gen.addEventListener("click", generate_array);
 inp_as.addEventListener("input", update_array_size);
 
 window.onload = update_array_size(); 
 
-//Array generation and updation
+// Array generation and updation
 function generate_array()
 {
-    cont.innerHTML="";  
+    cont.innerHTML = "";  
     for(var i = 0; i < array_size; i++)
     {
-        div_sizes[i]=Math.floor((Math.random() * 60) + 10);
-        divs[i]=document.createElement("div");
+        div_sizes[i] = Math.floor((Math.random() * 60) + 10);
+        divs[i] = document.createElement("div");
         cont.appendChild(divs[i]);
         if(array_size > 30){
             divs[i].innerHTML = "";
@@ -35,7 +34,7 @@ function generate_array()
         else{
             divs[i].innerHTML = div_sizes[i];
             divs[i].classList.add("blocks");    
-            if(array_size<10){
+            if(array_size < 10){
                 margin_size = 0.4;  
             }
             else{
@@ -43,7 +42,7 @@ function generate_array()
             }
         }
         
-        divs[i].style=" margin:0% " + margin_size + "%; background-color: #F08080; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+        divs[i].style = " margin:0% " + margin_size + "%; background-color: #F08080; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
     }
 }
 
@@ -67,11 +66,11 @@ function update(){
     drop_button.innerHTML = algorithm;
 }
     
-// Disabling buttons during execution
+// Disabling buttons before execution
 function disable_functions(){
-    inp_as.disabled=true;
-    inp_aspeed.disabled=true;
-    inp_gen.removeEventListener("click",generate_array);
+    inp_as.disabled = true;
+    inp_aspeed.disabled = true;
+    inp_gen.removeEventListener("click", generate_array);
     inp_gen.classList.add("butt_locked");
     inp_gen.removeAttribute("id");
     sort_button.removeEventListener("click",runalgo);
@@ -83,7 +82,7 @@ function disable_functions(){
     drop_button.style.cursor = "default";
 }
 
-//Running the appropriate algorithm
+// Running the appropriate algorithm
 function runalgo()
 {
     if(algorithm == undefined){
